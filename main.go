2e5cd4eb55151281b4eb1551360b5b42547fc626
Kingdom Orjiewuru <kingdom.orjiewuru@andela.com>
2020-01-13 17:09:30 +0100

    feat: basic template setup

--- main.go
+++ main.go
@@ -1,22 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
 
-import "log"
+// templ - single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Let's chat!
-				</body>
-			</html>
-		`))
-	})
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// start the web server or log error
 	if err := http.ListenAndServe(":8080", nil); err != nil {
