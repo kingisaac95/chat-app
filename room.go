eba5984d24f3f48011c37d95273498b87d53e30e
Kingdom Orjiewuru <kingdom.orjiewuru@andela.com>
2020-01-18 18:24:45 +0100

    feat: make an HTTP handler for room

--- room.go
+++ room.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
 type room struct {
 	// forward: a channel that holds incoming messages
 	// that should be forwarded to the other clients.
@@ -28,4 +34,38 @@ func (r *room) run() {
 			}
 		}
 	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+// websocket.Upgrader type upgrades our HTTP connection to use web sockets
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// use Upgrade method from the web socket type to get the socket from a HTTP request
+	socket, err := upgrader.Upgrade(w, req, nil)
+
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	// create a client from the socket
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	// add the client to the current room
+	r.join <- client
+	defer func() { r.leave <- client }()
+	// run the client.write method in a different goroutine(thread)
+	go client.write()
+	// read from the main thread keeping the connection alive until closed
+	client.read()
 }
