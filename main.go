ddc2e83957e09d5672854424f1de95e7ae03d007
Kingdom Orjiewuru <kingdom.orjiewuru@andela.com>
2020-01-19 17:42:26 +0100

    feat: add new room helper and initialize room in goroutine

--- main.go
+++ main.go
@@ -23,8 +23,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// initialize the room
+	go r.run()
 
 	// start the web server or log error
 	if err := http.ListenAndServe(":8080", nil); err != nil {
--- room.go
+++ room.go
@@ -18,6 +18,16 @@ type room struct {
 	clients map[*client]bool
 }
 
+// newRoom helper for creating rooms
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
+
 // listen for messages in any of the three channels
 // and execute matching case via select
 func (r *room) run() {
